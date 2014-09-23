0671935780c9903e1e6e2e2f6c7a602694675234
akassay <arkassay@gmail.com>
2014-09-23 16:01:00 -0400

    first app view created - successful debug and deploy to emulator

--- research/HealthyAtWork/bin/classes.dex.d
+++ research/HealthyAtWork/bin/classes.dex.d
@@ -1,5 +1,6 @@
 /Users/akassay/Desktop/Work/GIT/sciencefair/research/HealthyAtWork/bin/classes.dex : \
 /Users/akassay/Desktop/Work/GIT/sciencefair/research/HealthyAtWork/bin/classes/com/example/healthyatwork/BuildConfig.class \
+/Users/akassay/Desktop/Work/GIT/sciencefair/research/HealthyAtWork/bin/classes/com/example/healthyatwork/DisplayMessageActivity.class \
 /Users/akassay/Desktop/Work/GIT/sciencefair/research/HealthyAtWork/bin/classes/com/example/healthyatwork/MainActivity.class \
 /Users/akassay/Desktop/Work/GIT/sciencefair/research/HealthyAtWork/bin/classes/com/example/healthyatwork/R$attr.class \
 /Users/akassay/Desktop/Work/GIT/sciencefair/research/HealthyAtWork/bin/classes/com/example/healthyatwork/R$drawable.class \
--- research/HealthyAtWork/src/com/example/healthyatwork/MainActivity.java
+++ research/HealthyAtWork/src/com/example/healthyatwork/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.healthyatwork;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
+    public final static String EXTRA_MESSAGE = "com.example.healthyatwork.MESSAGE";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -16,6 +22,10 @@ public class MainActivity extends Activity
     /** Called when the user clicks the Send button */
     public void sendMessage(View view)
     {
-
+      Intent intent = new Intent(this, DisplayMessageActivity.class);
+      EditText editText = (EditText) findViewById(R.id.edit_message);
+      String message = editText.getText().toString();
+      intent.putExtra(EXTRA_MESSAGE, message);
+      startActivity(intent);
     }
 }
